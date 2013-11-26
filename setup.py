7548e9b79532933570917ab453f0fbb0fd03748f
Robert Elwell <robert@wikia-inc.com>
2013-11-25 17:14:41 -0800

    now can be easy installed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="mrg_utils",
+    version= "0.0.1",
+    author = "Robert Elwell",
+    author_email = "robert.elwell@gmail.com",
+    description = "A set of utilities for interacting with Penn-Treebank .mrg-formatted parses and identifying syntactic heads",
+    license = "GPL",
+    packages = ["mrg_utils"],
+    depends = []
+    )
