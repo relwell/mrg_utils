ae26c832b95b918b4aa11ee3b940e6c4a40d4afe
Robert Elwell <robert@wikia-inc.com>
2014-02-05 16:07:49 -0800

    setup.py stuff

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     author_email="robert.elwell@gmail.com",
     description="A set of utilities for interacting with Penn-Treebank .mrg-formatted parses and identifying syntactic heads",
     license="GPL",
-    packages=["mrg_utils", "mrg_utils.sentence"],
+    packages=["mrg_utils", "mrg_utils.sentence", "mrg_utils.cuecorp", "mrg_utils.docNode", 
+              "mrg_utils.headRules", "mrg_utils.mrg_document",
+              "mrg_utils.node", "mrg_utils.nonTerminalNode", "mrg_utils.rootNode", "mrg_utils.sentence", 
+              "mrg_utils.sexpr_parse", "mrg_utils.terminalNode"],
     )
