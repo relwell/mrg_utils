4ebdc1b20f7717bf5fa59ac5a712b4745a3e5a2a
Robert Elwell <robert@wikia-inc.com>
2014-06-04 14:06:00 -0700

    added github url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="mrg_utils",
     version="0.0.1",
     author="Robert Elwell",
+    url="https://github.com/relwell/mrg_utils",
     author_email="robert.elwell@gmail.com",
     description="A set of utilities for interacting with Penn-Treebank .mrg-formatted parses and identifying syntactic heads",
     license="GPL",
